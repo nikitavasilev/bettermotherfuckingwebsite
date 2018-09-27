4273a7904495a1806b84cef4cf9b33a40d75e895
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-09-27 20:23:19 +0200

    add mode psychedelic

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function popup() {
     var txt;
     if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
         txt = "You pressed OK!";
@@ -9,5 +9,53 @@ function myFunction() {
 }
 
 setTimeout(function() {
-	myFunction();
+	popup();
 }, 10000);
+
+function changeBackground() {
+	document.body.style.backgroundColor = "magenta";
+}
+
+function changeColorP() {
+	var y = document.getElementsByTagName("P");
+	var i;
+	for (i = 0; i < y.length; i++) {
+    	y[i].style.color = "blue";
+	}
+}
+ 
+var headings = document.querySelectorAll("h1,h2,h3");
+
+function changeColorH() {
+	var i;
+
+	for (i = 0; i < headings.length; i++) {
+    	headings[i].style.color = "green";
+	}
+}
+
+function changeFontP() {
+	var y = document.getElementsByTagName("P");
+	var i;
+	for (i = 0; i < y.length; i++) {
+    	y[i].style.fontFamily = "papyrus";
+	}
+}
+
+function changeFontH() {
+	var y = document.getElementsByTagName("H1");
+	var i;
+	for (i = 0; i < y.length; i++) {
+    	y[i].style.fontFamily = "Comic Sans";
+	}
+}
+
+var myButton = document.querySelector('button');
+
+myButton.onclick = function() {
+  changeBackground();
+  changeColorP();
+  changeColorH();
+  changeFontP();
+  changeFontH();
+}
